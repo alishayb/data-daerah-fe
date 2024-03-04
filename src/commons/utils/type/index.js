d6c0d764c80bc785322507cba932cf44c30c52a1
alishayb <alishayumnab@gmail.com>
2024-03-04 09:54:03 +0700

    add type utility

---
+++ src/commons/utils/type/index.js
@@ -0,0 +1,42 @@
+const PROVINSI = {
+  type: "provinsi",
+  name: "Provinsi",
+};
+
+const KABUPATEN_KOTA = {
+  name: "Kabupaten / Kota",
+  type: "kabupaten_kota",
+};
+
+const KECAMATAN = {
+  name: "Kecamatan",
+  type: "kecamatan",
+};
+
+const getType = (tipe) => {
+  switch (tipe) {
+    case "provinsi":
+      return {
+        name: PROVINSI.name,
+        parent: null,
+        child: KABUPATEN_KOTA,
+      };
+
+    case "kabupaten_kota":
+      return {
+        name: KABUPATEN_KOTA.name,
+        parent: PROVINSI,
+        child: KECAMATAN,
+      };
+
+    case "kecamatan":
+      return {
+        name: KECAMATAN.name,
+        parentType: KABUPATEN_KOTA,
+        childType: null,
+      };
+      
+    default:
+      break;
+  }
+};
