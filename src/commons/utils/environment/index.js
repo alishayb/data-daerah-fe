3aee43ac62694a2c6f5cb142ed2f922c4a6327d9
alishayb <alishayumnab@gmail.com>
2024-03-04 10:05:26 +0700

    fix env

--- src/commons/utils/environment/index.js
+++ src/commons/utils/environment/index.js
@@ -1,7 +1,7 @@
 const DOMAIN = process.env.NEXT_PUBLIC_URL;
-const API = process.env.DATA_DAERAH_API;
+const API = process.env.NEXT_PUBLIC_DATA_DAERAH_API;
 
 console.log(`DOMAIN: ${DOMAIN}`)
-console.log(`DATA_DAERAH_API: ${DATA_DAERAH_API}`)
+console.log(`DATA_DAERAH_API: ${API}`)
 
 export { DOMAIN, API };
